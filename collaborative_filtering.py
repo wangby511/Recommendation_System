4984b8e049abdc6c713e4ed48850aed9c9cfe8c3
wangby511 <wangby511@gmail.com>
2019-09-04 14:27:50 -0700

    20190904 clf_class

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,21 +1,18 @@
 #-*-coding:utf-8-*-
 
 import numpy as np
-import _pickle as cPickle
-INPUT_FILE_NAME = 'popular_music_suprise_format.txt'
 
-
-class collaborative_class_wrapper:
+class collaborative_filter_wrapper:
     def __init__(self, INPUT_FILE_NAME):
         self.file_path = INPUT_FILE_NAME
-        self.load_data()
+        self.init_load_data()
 
-    def load_data(self):
+    def init_load_data(self):
         self.users_list = []
         self.users = dict()
         self.songs = dict()
 
-        origin_file = open(INPUT_FILE_NAME, 'r')
+        origin_file = open(self.file_path, 'r')
         for line in origin_file:
             contents = line.strip().split(",")
             user = contents[0]
@@ -82,50 +79,4 @@ class collaborative_class_wrapper:
         top_k_recommend_list = top_k_recommend_list[:10]
 
         print("top_k_recommend_list =",top_k_recommend_list)
-
         return top_k_recommend_list
-
-if __name__ == "__main__":
-    cl_filter = collaborative_class_wrapper(INPUT_FILE_NAME)
-    current_playlist = '75050697'
-    current_playlist = '326644112'
-    playlist_neighbors = cl_filter.nearestNeighbors(current_playlist)
-
-    id_name_dic = cPickle.load(open('popular_playlist.pkl', 'rb'), encoding='utf-8')
-    print("加载歌单id到歌单名的映射字典")
-
-    name_id_dic = {}
-    for playlist_id in id_name_dic:
-        name_id_dic[id_name_dic[playlist_id]] = playlist_id
-    print("加载歌单名到歌单id的映射字典")
-
-    playlist_neighbors = (id_name_dic[playlist_id]
-                          for playlist_id in playlist_neighbors)
-
-    print()
-    print("和歌单 《", id_name_dic[current_playlist], "》 最接近的10个歌单为：\n")
-    for playlist in playlist_neighbors:
-        print(playlist)
- # 当过千评论的华语翻唱遇上“原唱”【更新】
-# 【华语】暖心物语 纯白思念
-# 〖循环〗单曲循环是强迫症吗？
-# 周杰伦地表最强演唱会2017520南京站曲目
-# 简单的爱总是那么吸引人
-# 『华语/回忆』95后陪伴我中学时期的歌曲
-# 对不起，你是一个好人
-# 所有的大人，曾经都是小孩
-# 有没有一首歌让你泪流满面
-# 专属你的周杰伦
-
-# 和歌单 《 不悔梦归处，只怨太匆匆。 》 最接近的10个歌单为：
-#
-# 当过千评论的华语翻唱遇上“原唱”【更新】
-# 【华语】暖心物语 纯白思念
-# 周杰伦地表最强演唱会2017520南京站曲目
-# 『华语/回忆』95后陪伴我中学时期的歌曲
-# 暗暗作祟| 不甘朋友不敢恋人
-# 专属你的周杰伦
-# 你知道思念一个人的滋味吗
-# 「华语歌曲」
-# 愿形容我的词 别太荒唐
-# 致我们终将逝去的青春（华语精选）
---
+++ film_predict.py
@@ -0,0 +1,61 @@
+import os
+import io
+
+from surprise import KNNBaseline
+from surprise import Dataset
+
+
+def read_item_names():
+    """
+    获取电影名到电影id 和 电影id到电影名的映射
+    """
+
+    file_name = './ml-100k/u.item'
+    rid_to_name = {}
+    name_to_rid = {}
+    with io.open(file_name, 'r', encoding='ISO-8859-1') as f:
+        for line in f:
+            line = line.split('|')
+            rid_to_name[line[0]] = line[1]
+            name_to_rid[line[1]] = line[0]
+
+    return rid_to_name, name_to_rid
+
+data = Dataset.load_builtin('ml-100k')
+trainset = data.build_full_trainset()
+sim_options = {'name': 'pearson_baseline', 'user_based': False}
+algo = KNNBaseline(sim_options=sim_options)
+
+# [STDOUT IN COMMAND LINES]
+# Estimating biases using als...
+# Computing the pearson_baseline similarity matrix...
+# Done computing similarity matrix.
+
+# UserWarning: train() is deprecated. Use fit() instead warnings.warn('train() is deprecated. Use fit() instead', UserWarning)
+# algo.train(trainset)
+algo.fit(trainset)
+
+rid_to_name, name_to_rid = read_item_names()
+
+origin_film_name = 'GoldenEye (1995)'
+toy_story_raw_id = name_to_rid[origin_film_name]
+# print(toy_story_raw_id)
+
+toy_story_inner_id = algo.trainset.to_inner_iid(toy_story_raw_id)
+# print(toy_story_inner_id)
+# 一个电影在algo内部的id, 并不一定等于文件中的id
+
+# 找到最近的k = 10个邻居
+K = 10
+toy_story_neighbors = algo.get_neighbors(toy_story_inner_id, k=K)
+print(toy_story_neighbors)
+
+# 从近邻的id映射回电影名称
+toy_story_neighbors = (algo.trainset.to_raw_iid(inner_id)
+                       for inner_id in toy_story_neighbors)
+toy_story_neighbors = (rid_to_name[rid]
+                       for rid in toy_story_neighbors)
+
+print('The ' + str(K) + ' nearest neighbors of ' + origin_film_name + ' are:')
+for movie in toy_story_neighbors:
+    print(movie)
